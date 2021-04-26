6f486a5e3c16cca981bfa354ee8e68a2dc8c03db
chrzyki <christoph@functor-argument.net>
2021-04-26 13:18:28 +0200

    Update package requirements

--- setup.py
+++ setup.py
@@ -2,25 +2,18 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json', encoding='utf-8') as fp:
+with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_asjp',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_asjp'],
+    name="lexibank_asjp",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_asjp"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'asjp=lexibank_asjp:Dataset',
-        ]
-    },
-    install_requires=[
-        'pyasjp>=1.1',
-        'pylexibank>=2.1',
-    ]
+    entry_points={"lexibank.dataset": ["asjp=lexibank_asjp:Dataset"]},
+    install_requires=["pyasjp>=1.1", "pylexibank>=3.0", "openpyxl>=3.0", "xlrd>=2.0"],
 )
